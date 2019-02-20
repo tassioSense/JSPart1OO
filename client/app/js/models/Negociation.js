b215502814ff4c9a2f3093138235984148341052
Tássio Jordão <tassio72@hotmail.com>
2019-02-19 23:59:19 -0300

    Boas praticas OO

---
+++ client/app/js/models/Negociation.js
@@ -0,0 +1,38 @@
+class Negociation {
+
+    constructor(data, quantidade,valor){
+        //o _ , por covenção, simboliza um atributo private
+
+        this._data = new Data(data.getTime()); //instanciando um novo objeto para garantir que o usuário não irá alterar a data
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        Object.freeze(this); //encapsulando a instancia
+
+    }
+
+    get data(){
+
+        return new Date(this._data.getTime()); //instanciando um novo objeto para garantir que o usuário não irá alterar a data
+
+    }
+    get quantidade(){
+
+        return this._quantidade;
+    
+    }
+    get valor(){
+
+        return this._valor;
+
+    }
+
+    get volume(){
+
+        return this._quantidade * this._valor;
+   
+    }
+    
+    
+    
+}
