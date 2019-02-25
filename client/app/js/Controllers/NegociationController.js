f24f56e8866846f448a2a863615511769ce9a5f6
Tássio Jordão <tassio72@hotmail.com>
2019-02-24 23:07:49 -0300

    Dando um GET no servidor

--- client/app/js/Controllers/NegociationController.js
+++ client/app/js/Controllers/NegociationController.js
@@ -41,6 +41,23 @@ class NegociationController {
 
     }
 
+    importaNegociacoes() {
+
+        let service = new NegociacaoService();
+
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+      
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+        });
+
+        
+    }
+
     _criaNegociacao() {
         //instanciando uma Negociação
         
